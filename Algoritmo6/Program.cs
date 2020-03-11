4433a8b71e6c029a2a700d24ebb495a5f29426ef
wilder3756 <60258334+wilder3756@users.noreply.github.com>
2020-03-11 12:28:57 -0500

    Add files via upload

--- Algoritmo6/Program.cs
+++ Algoritmo6/Program.cs
@@ -16,12 +16,22 @@ namespace Algoritmo6
                 char inicio , comienzo;
                 Console.WriteLine("Ingrese la cadena:");
                 cadena = Console.ReadLine();
+                Console.WriteLine("\nParte 1");
                 Console.WriteLine("Ingrese la letra de inicio:");
                 inicio = Convert.ToChar(Console.ReadLine());
                 Console.WriteLine("Ingrese la letra con la que comienza:");
                 comienzo = Convert.ToChar(Console.ReadLine());
                 Cifrador ejm = new Cifrador(cadena, inicio, comienzo);
                 Console.WriteLine("La cadena original es: " + cadena + "    la cadena ya cifrada es: " + ejm.Cadena);
+
+                Console.WriteLine("\n\nParte 2");
+                Console.WriteLine("Ingrese la letra de inicio:");
+                inicio = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("Ingrese la letra con la que comienza:");
+                comienzo = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("La cadena cifrada es: " + ejm.Cadena );
+                ejm.decifrar(inicio,comienzo);
+                Console.Write("la cadena ya decifrada es: " + ejm.Cadena+"\n");
                 Console.ReadKey();
             }
             catch (Exception e)
@@ -32,43 +42,64 @@ namespace Algoritmo6
         }
             public class Cifrador
             {
-            public static char[] abc = new char[26] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'} ;
-            private string cadena;
+                public static char[] abc = new char[26] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'} ;
+                private string cadena;
 
-            public Cifrador(string cadena, char inicio,char comienzo)
-            {
-                this.cadena = cadena.ToLower();
-                inicio = Convert.ToChar(inicio.ToString().ToLower());
-                comienzo = Convert.ToChar(comienzo.ToString().ToLower());
-                cifrar(inicio, comienzo);
-            }
+                public Cifrador(string cadena, char inicio,char comienzo)
+                {
+                    this.cadena = cadena.ToLower();
+                    inicio = Convert.ToChar(inicio.ToString().ToLower());
+                    comienzo = Convert.ToChar(comienzo.ToString().ToLower());
+                    cifrar(inicio, comienzo);
+                }
 
-            public string Cadena { get => cadena; set => cadena = value; }
+                public string Cadena { get => cadena; set => cadena = value; }
 
-            public void cifrar(char inicio, char comienzo)
+                public void cifrar(char inicio, char comienzo)
+                {
+                    int posiciones = 0;
+                    string aux="";
+                    if(Convert.ToInt32(inicio)< Convert.ToInt32(comienzo))posiciones = Convert.ToInt32(comienzo) - Convert.ToInt32(inicio);
+                    else posiciones = Convert.ToInt32(inicio) - Convert.ToInt32(comienzo);
+                    for(int i = 0; i < cadena.Length; i++)
+                    {
+                        char letra = cadena[i];
+                        if (Convert.ToInt32(letra) + posiciones <= 122)
+                        {
+                            aux += Convert.ToString(Convert.ToChar(Convert.ToInt32(letra) + posiciones));
+                        }
+                        else
+                        {
+
+                            aux += Convert.ToString(Convert.ToChar(96+((Convert.ToInt32(letra) + posiciones)-122)));
+                        }
+                    }
+                    this.cadena = aux;
+                
+                }
+            public void decifrar(char inicio, char comienzo)
             {
                 int posiciones = 0;
-                string aux="";
-                if(Convert.ToInt32(inicio)< Convert.ToInt32(comienzo))posiciones = Convert.ToInt32(comienzo) - Convert.ToInt32(inicio);
+                string aux = "";
+                if (Convert.ToInt32(inicio) < Convert.ToInt32(comienzo)) posiciones = Convert.ToInt32(comienzo) - Convert.ToInt32(inicio);
                 else posiciones = Convert.ToInt32(inicio) - Convert.ToInt32(comienzo);
-                for(int i = 0; i < cadena.Length; i++)
+
+                for (int i = 0; i < cadena.Length; i++)
                 {
                     char letra = cadena[i];
-                    if (Convert.ToInt32(letra) + posiciones <= 122)
+                    if (Convert.ToInt32(letra) - posiciones >=97)
                     {
-                        aux += Convert.ToString(Convert.ToChar(Convert.ToInt32(letra) + posiciones));
+                        aux += Convert.ToString(Convert.ToChar(Convert.ToInt32(letra) - posiciones));
                     }
                     else
                     {
-                        aux += Convert.ToString(Convert.ToChar(96+((Convert.ToInt32(letra) + posiciones)-122)));
+                        Console.WriteLine(".");
+                        aux += Convert.ToString(Convert.ToChar( 122 - (96-(Convert.ToInt32(letra) - posiciones))));
                     }
                 }
                 this.cadena = aux;
-                
             }
-
-            
-        }
+            }
         
     }
 }
