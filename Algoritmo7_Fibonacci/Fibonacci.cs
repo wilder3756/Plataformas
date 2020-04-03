e94b54f7e276b2d122f4c43c8fc7373a3c9f0664
wilder3756 <60258334+wilder3756@users.noreply.github.com>
2020-04-03 11:39:37 -0500

    Algoritmo Fibonacci
    Se calcula el Fibonacci n y se imprimien los n primeros numeros de la sesir de Fibonacci en orden descendente

---
+++ Algoritmo7_Fibonacci/Fibonacci.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritmo7_Fibonacci
+{
+    class Fibonacci
+    {
+        private long numeroSerie;
+        private long numeroSerieAnterior;
+        private long n;
+        private long iterador;
+
+        public Fibonacci()
+        {
+        }
+
+        public Fibonacci(int n)
+        {
+            this.numeroSerieAnterior= 0;
+            this.n = n;
+            calcularFibonacci();
+
+        }
+
+        public long NumeroSerie { get => numeroSerie; set => numeroSerie = value; }
+        public long N { get => n; set => n = value; }
+
+        private void calcularFibonacci()
+        {
+            long aux;
+            numeroSerie = 0;
+            numeroSerieAnterior=1;
+            for (long i= 0; i < n; i++)
+            {
+                aux = numeroSerie;
+                numeroSerie = numeroSerieAnterior;
+                numeroSerieAnterior = aux + numeroSerie;
+            }
+            
+        }
+        private long[] calcularFibonacciDESC()
+        {
+            long[] result = new long[n];
+            long aux;
+            numeroSerie = 0;
+            numeroSerieAnterior = 1;
+            for (int i = 0; i < n; i++)
+            {
+                aux = numeroSerie;
+                numeroSerie = numeroSerieAnterior;
+                numeroSerieAnterior = aux + numeroSerie;
+                result[i] = numeroSerie;
+            }
+            long[] desc=new long[n];
+            for (int i = 0; i < n; i++) { desc[i] = result[n - i - 1]; }
+            return desc;
+        }
+        public string imprimir()
+        {
+            string result = "Estos son los " + n + " numeros de Fibonacci en orden descendente\n";
+            foreach (long i in this.calcularFibonacciDESC())
+            {
+                result += " | " + i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Algoritmo7_Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Algoritmo7_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n;
+                Console.Write("¿Cual numero de la serie Fibonacci desea conocer? --> ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                Fibonacci f = new Fibonacci(n);
+                Console.WriteLine(f.imprimir());
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error genral: " + e.Message);
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
