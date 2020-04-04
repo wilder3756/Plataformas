17d358a2ed989f6201efee84b1fb127ff55860a0
wilder3756 <60258334+wilder3756@users.noreply.github.com>
2020-04-03 23:09:29 -0500

    Algoritmo Entre Palabras
    la entrada de este algoritmo son 3 palabras, la idea es verificar si el la palabra intermedia esta en el rango de inicial y final, mediante comprobacion elemento a elemento. Se utiliza POO con una clase llamada EntrePalabra que esta en el program en el mismo namespace.

---
+++ Algoritmo8_EntrePalabras/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo8_EntrePalabras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string inicio = "", final = "", intermedio = "";
+                Console.Write("Ingrese la palabra inicial -->");
+                inicio = Console.ReadLine();
+                Console.Write("Ingrese la palabra final -->");
+                final = Console.ReadLine();
+                Console.Write("Ingrese la palabra -->");
+                intermedio = Console.ReadLine();
+                EntrePalabra ePalabra = new EntrePalabra(inicio, final);
+                bool i = ePalabra.comprobarPalabra(intermedio);
+                if (i) Console.WriteLine("La cadena esta entre inicio y final: " + inicio + " -- " + intermedio + " -- " + final);
+                else Console.WriteLine("La cadena  NO esta entre inicio y final: " + inicio + " -- " + intermedio + " -- " + final);
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error General: " + e.Message);
+                Console.ReadKey();
+            }
+        }
+
+        public class EntrePalabra
+        {
+            private string inicio;
+            private string final;
+
+            public EntrePalabra(string inicio, string final)
+            {
+                this.inicio = inicio ?? throw new ArgumentNullException(nameof(inicio));
+                this.final = final ?? throw new ArgumentNullException(nameof(final));
+            }
+
+            public string Inicio { get => inicio; set => inicio = value; }
+            public string Final { get => final; set => final = value; }
+
+            public bool comprobarPalabra(string palabra)
+            {
+                char[] cadenaInicio = inicio.ToLower().ToCharArray();
+                char[] cadenaFinal = final.ToLower().ToCharArray();
+                char[] cadenaIntermedia = palabra.ToLower().ToCharArray();
+
+                int length = (cadenaInicio.Length < cadenaFinal.Length) ? cadenaInicio.Length : cadenaFinal.Length;
+                for(int i = 0; i < length; i++)
+                {
+                    if (i == cadenaFinal.Length || i == cadenaInicio.Length || i == cadenaIntermedia.Length) return false;
+                    if (cadenaInicio[i] == cadenaIntermedia[i] && cadenaIntermedia[i] == cadenaIntermedia[i]) continue;
+                    else if (cadenaIntermedia[i] < cadenaFinal[i] && cadenaIntermedia[i] > cadenaInicio[i]) return true;
+                    else if (cadenaIntermedia[i] < cadenaFinal[i] && cadenaFinal[i] < cadenaInicio[i]) return true;
+                    else if (cadenaIntermedia[i] < cadenaFinal[i] && cadenaFinal[i] < cadenaInicio[i]) return true;
+                    else return false;
+
+                }
+                return false;
+
+            }
+        }
+    }
+}
